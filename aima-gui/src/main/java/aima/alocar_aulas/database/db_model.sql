9c4b9d10f7db86fa3cea0c2bc223d9e827273434
Felipe Passos <felipekaires@hotmail.com>
2020-02-21 16:20:18 -0300

    Update db_model.sql

--- aima-gui/src/main/java/aima/alocar_aulas/database/db_model.sql
+++ aima-gui/src/main/java/aima/alocar_aulas/database/db_model.sql
@@ -21,3 +21,29 @@ create table Preferencias (
                                   ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
+
+insert into professor (nome) values ('Walter');
+insert into professor (nome) values ('Elena');
+insert into professor (nome) values ('Evelyn');
+insert into professor (nome) values ('Mia');
+insert into professor (nome) values ('Robert');
+
+
+
+insert into habilidades (disciplina,professor_id)  values ('Banco de Dados I',1);
+insert into habilidades (disciplina,professor_id)  values ('Eletronica I',1);
+insert into habilidades (disciplina,professor_id)  values ('Engenharia de Software II',1);
+insert into habilidades (disciplina,professor_id)  values ('Inteligencia Artificial',2);
+insert into habilidades (disciplina,professor_id)  values ('Laboratorio de Redes de Computadores',2);
+insert into habilidades (disciplina,professor_id)  values ('Programacao Paralela e Concorrente",
+			"Sistemas Distribuidos',2);
+insert into habilidades (disciplina,professor_id)  values ('Banco de Dados I',3);
+insert into habilidades (disciplina,professor_id)  values ('Eletronica I',3);
+insert into habilidades (disciplina,professor_id)  values ('Engenharia de Software II',3);
+insert into habilidades (disciplina,professor_id)  values ('Inteligencia Artificial',4);
+insert into habilidades (disciplina,professor_id)  values ('Laboratorio de Redes de Computadores',4);
+insert into habilidades (disciplina,professor_id)  values ('Programacao Paralela e Concorrente",
+			"Sistemas Distribuidos',4);
+insert into habilidades (disciplina,professor_id)  values ('Banco de Dados I',5);
+insert into habilidades (disciplina,professor_id)  values ('Eletronica I',5);
+insert into habilidades (disciplina,professor_id)  values ('Engenharia de Software II',5);
