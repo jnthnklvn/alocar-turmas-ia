f25a603c00dc821b3fdf8e114e6300fb0d9a4a06
Felipe Passos <felipekaires@hotmail.com>
2020-02-21 11:52:48 -0300

    change db_model

--- aima-gui/src/main/java/aima/alocar_aulas/database/db_model.sql
+++ aima-gui/src/main/java/aima/alocar_aulas/database/db_model.sql
@@ -1,16 +1,23 @@
 create Table Professor (
-	id  serial PRIMARY KEY, 
-	nome  VARCHAR (200) NOT NULL,
-	preferencia_disciplina  VARCHAR (50) NOT NULL,
-	preferencia_horario VARCHAR (50),
-	preferencia_dia  VARCHAR (50)
+                           id  serial PRIMARY KEY,
+                           nome  VARCHAR (200) NOT NULL
 );
 
 create table Habilidades (
-	id  serial PRIMARY KEY, 
-	disciplinas  VARCHAR (200) NOT NULL,
-	professor_id BIGINT NOT NULL,
-	CONSTRAINT professor_fkey FOREIGN KEY (professor_id)
-    REFERENCES Professor (id) MATCH SIMPLE
-    ON UPDATE NO ACTION ON DELETE NO ACTION
+                             id  serial PRIMARY KEY,
+                             disciplina  VARCHAR (200) NOT NULL,
+                             professor_id BIGINT NOT NULL,
+                             CONSTRAINT professor_fkey FOREIGN KEY (professor_id)
+                                 REFERENCES Professor (id) MATCH SIMPLE
+                                 ON UPDATE NO ACTION ON DELETE NO ACTION
 );
+
+create table Preferencias (
+                              id  serial PRIMARY KEY,
+                              disciplina  VARCHAR (200) NOT NULL,
+                              professor_id BIGINT NOT NULL,
+                              CONSTRAINT professor_fkey FOREIGN KEY (professor_id)
+                                  REFERENCES Professor (id) MATCH SIMPLE
+                                  ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
